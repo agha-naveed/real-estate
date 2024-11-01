b9333adaa3856bdff51553e0a9519499d1c70a7b
Agha Naveed <naveedabs31@gmail.com>
2024-11-01 14:51:28 +0500

    .

--- src/NewProperty.jsx
+++ src/NewProperty.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Form } from 'react-router-dom'
 
 export default function NewProperty() {
   return (
-    <div>NewProperty</div>
+    <div>
+      <Form method='POST'>
+        <div>
+          <label htmlFor="p_type">Property Type</label>
+          <select name="" id="">
+            <option value="">Residential</option>
+            <option value="">Commercial</option>
+            <option value="">Land</option>
+            <option value="">Mix-Use</option>
+          </select>
+        </div>
+      </Form>
+      
+    </div>
   )
 }
