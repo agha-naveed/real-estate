b1e81d7cd257f4e1f74b8e8c3f317be91aa7ce29
Agha Naveed <naveedabs31@gmail.com>
2024-11-06 15:25:23 +0500

    Changes in invoice component

--- src/NewInvoice.jsx
+++ src/NewInvoice.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useInsertionEffect, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Form, useNavigate } from 'react-router-dom'
 
@@ -10,7 +10,52 @@ export default function NewInvoice() {
     const {register, handleSubmit } = useForm()
   
     const navigate = useNavigate()
+
   
+    const [selectedPropertyId, setSelectedPropertyId] = useState('');
+    const [sellerId, setSellerId] = useState('');
+    const [payableAmount, setPayableAmount] = useState('');
+
+    // Getting Data
+
+
+    const [userData, setUserData] = useState([]);
+  
+
+
+    useInsertionEffect(() => {
+      let propertyData = async () => {
+        try {
+          let response = await axios.get("http://localhost:7000/api/property-details");
+          setUserData(response.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      propertyData();
+    }, []);
+    
+
+    useEffect(() => {
+      if (selectedPropertyId) {
+          let propertyData = async () => {
+            let response = await axios.get(`http://localhost:7000/api/property-details/${selectedPropertyId}`);
+              setSellerId(response.data.seller_id);
+              setPayableAmount(response.data.property_price);
+            }
+      }
+    }, [selectedPropertyId]);
+
+
+  const handlePropertyChange = (e) => {
+    setSelectedPropertyId(e.target.value);
+};
+
+    // Getting Data Ended
+
+
+
+
     const onSubmit = (data) => {
       console.log(data)
       const res = axios.post("http://localhost:7000/api/create-new-invoice", data)
@@ -28,13 +73,66 @@ export default function NewInvoice() {
       }
     }
   
-    
   
+    // return (
+    //   <Form onSubmit={handleSubmit(onSubmit)} method='POST' className='add-property-form xl:w-[600px] lg:w-[70%] w-full py-10 px-8'>
+    //     <div>
+    //       <label htmlFor="prp-id">Property ID</label>
+    //       <input type="number" id='prp-id' {...register("property_id")} required />
+    //     </div>
+        
+    //     <div className='md:flex grid md:gap-5'>
+    //         <div>
+    //         <label htmlFor="buy-id">Buyer ID</label>
+    //         <input type="number" id='buy-id' {...register("buyer_id")} required />
+    //         </div>
+    
+    //         <div>
+    //         <label htmlFor="sell-id">Seller ID</label>
+    //         <input type="number" id='sell-id' {...register("seller_id")} required />
+    //         </div>
+    //     </div>
+
+    //     <div className='md:flex grid md:gap-5'>
+    //         <div>
+    //         <label htmlFor="in-rec-amount">Recievable Amount</label>
+    //         <input type="number" id='in-rec-amount' {...register("invoice_recievable_amount")} required />
+    //         </div>
+
+    //         <div>
+    //         <label htmlFor="in-pay-amount">Payable Amount</label>
+    //         <input type="number" id='in-pay-amount' {...register("invoice_payable_amount")} required />
+    //         </div>
+    //     </div>
+
+    //     <div>
+    //       <label htmlFor="in-comm-amount">Commission Amount</label>
+    //       <input type="number" id='in-comm-amount' {...register("commission_amount")} required />
+    //     </div>
+        
+  
+    //     <input type="submit" className='submit-btn mt-2' name='add-invoice-submit-btn' />
+  
+    //   </Form>
+    // )
+
+
+
     return (
       <Form onSubmit={handleSubmit(onSubmit)} method='POST' className='add-property-form xl:w-[600px] lg:w-[70%] w-full py-10 px-8'>
         <div>
           <label htmlFor="prp-id">Property ID</label>
-          <input type="number" id='prp-id' {...register("property_id")} required />
+          <select value={selectedPropertyId} onChange={handlePropertyChange}>
+              <option value="" defaultValue={true}>Select a property</option>
+            {
+              userData.map((item, index) => {
+                return (
+                  <option key={`p_id_selec_${index}`} value={item.property_id}>{item.property_id}</option>
+                )
+              })
+            }
+          </select>
+
         </div>
         
         <div className='md:flex grid md:gap-5'>
@@ -45,7 +143,7 @@ export default function NewInvoice() {
     
             <div>
             <label htmlFor="sell-id">Seller ID</label>
-            <input type="number" id='sell-id' {...register("seller_id")} required />
+            <input type="number" id='sell-id' value={sellerId} {...register("seller_id")} required readOnly />
             </div>
         </div>
 
@@ -57,7 +155,7 @@ export default function NewInvoice() {
 
             <div>
             <label htmlFor="in-pay-amount">Payable Amount</label>
-            <input type="number" id='in-pay-amount' {...register("invoice_payable_amount")} required />
+            <input type="number" id='in-pay-amount' value={payableAmount} {...register("invoice_payable_amount")} required readOnly  />
             </div>
         </div>
 
@@ -71,4 +169,6 @@ export default function NewInvoice() {
   
       </Form>
     )
+
+
 }
