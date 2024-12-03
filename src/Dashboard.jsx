f0a2a3316bab72b7fb144b977fe390f444d81858
Agha Naveed <naveedabs31@gmail.com>
2024-12-03 18:52:30 +0500

    adding dashboard featues

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import HeaderDiv from './HeaderDiv'
+import { PiHouseLine } from "react-icons/pi";
 
 export default function Dashboard() {
   return (
     <div className='w-full h-full'>
       <HeaderDiv value='Dashboard' />
       
+      <div className="card flex bg-red-500 items-center justify-between w-[220px] h-[90px] py-2 px-6">
+        <div className="grid text-white">
+          <span>Total Properties</span>
+          <span className='text-center text-2xl'>1</span>
+        </div>
+        <PiHouseLine className='text-[40px] text-white' />
+      </div>
     </div>
   )
 }
