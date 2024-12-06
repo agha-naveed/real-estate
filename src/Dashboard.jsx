7d7b98f128c0464f9e4a96bdf816b1c9d8ccbd78
Agha Naveed <naveedabs31@gmail.com>
2024-12-06 18:46:58 +0500

    ...

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useInsertionEffect, useState } from 'react'
 import HeaderDiv from './HeaderDiv'
 import { PiHouseLine } from "react-icons/pi";
+import axios from 'axios';
 
 export default function Dashboard() {
+
+  const [buyer, setBuyer] = useState('')
+
+  useInsertionEffect(() => {
+    let buyerDetails = async () => {
+      let resp = await axios.get("")
+    }
+  }, [])
+
   return (
     <div className='w-full h-full'>
       <HeaderDiv value='Dashboard' />
       
-      <div className="card flex bg-red-500 items-center justify-between w-[220px] h-[90px] py-2 px-6">
-        <div className="grid text-white">
-          <span>Total Properties</span>
-          <span className='text-center text-2xl'>1</span>
+      <div className="flex gap-6 p-5">
+        <div className="card flex bg-red-600 items-center justify-between w-[220px] h-[90px] py-2 px-6">
+          <div className="grid text-white">
+            <span>Total Properties</span>
+            <span className='text-center text-2xl'>1</span>
+          </div>
+          <PiHouseLine className='text-[40px] text-white' />
         </div>
-        <PiHouseLine className='text-[40px] text-white' />
+
+        <div className="card flex bg-blue-700 items-center justify-between w-[220px] h-[90px] py-2 px-6">
+          <div className="grid text-white">
+            <span>Total Buyers</span>
+            <span className='text-center text-2xl'>1</span>
+          </div>
+          <PiHouseLine className='text-[40px] text-white' />
+        </div>
+
+        <div className="card flex bg-orange-600 items-center justify-between w-[220px] h-[90px] py-2 px-6">
+          <div className="grid text-white">
+            <span>Total Sellers</span>
+            <span className='text-center text-2xl'>1</span>
+          </div>
+          <PiHouseLine className='text-[40px] text-white' />
+        </div>
       </div>
+
     </div>
   )
 }
